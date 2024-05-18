e40777f42d2e2bd78e4eb0941988d78ed210fa5a
SagaTheGreat <ajay.patel305@gmail.com>
2024-05-18 13:04:40 +0530

    updated.

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 def ring(request):
     return render(request, "ring.html")
@@ -49,6 +50,17 @@ def types(request):
     }
     return render(request, "types.html", context)
 
+
+def django_template_lang(request):
+    data = {
+        'sachin': 101,
+        'rony':55,
+        'jadeja':25,
+        'dube':43
+    }
+    color = random.choice(["blue", "red", "black", "yellow", "green", "pink"])
+    return render(request, "django_template_lang.html",context={"data":data, "color":color})
+
 urlpatterns = [
     path('ring/', ring),
     path('rotate/', render_with_context),
@@ -58,4 +70,5 @@ urlpatterns = [
     path('bar/', bar),
     path('login/', login),
     path('types/', types),
+    path('django_template_lang/', django_template_lang),
 ]
